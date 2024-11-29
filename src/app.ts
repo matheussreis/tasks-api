28db4a47301ce843e47e0e30e8b8a65e571a010c
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2024-11-29 16:12:35 +0000

    add basic cors setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { DBUtils } from './utils/database';
@@ -8,6 +9,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
 DBUtils.connect()
   .then(() => {
